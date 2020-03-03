9851af528c2300cfba72a57b86b3781d061ce908
dinismf <dinismarques95@gmail.com>
2020-03-03 14:23:51 +0000

    Cookie Cutter DS Template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A series of notebooks demonstrating core implementations of machine learning algorithms and techniques. ',
+    author='Dinis Firmino',
+    license='MIT',
+)
